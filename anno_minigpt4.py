17719eabf180b9d6a2c8b7d9f9f2c7023ceb0f70
ranxu <ranxu@ranxudeMacBook-Pro.local>
2023-12-19 16:45:26 +0800

    xr

--- anno_minigpt4.py
+++ anno_minigpt4.py
@@ -45,11 +45,6 @@ def setup_seeds(config):
     cudnn.benchmark = False
     cudnn.deterministic = True
 
-
-# ========================================
-#             Model Initialization
-# ========================================
-
 print('Initializing Chat')
 args = parse_args()
 cfg = Config(args)
@@ -63,10 +58,6 @@ vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
 vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
 chat = Chat(model, vis_processor, device='cuda:{}'.format(args.gpu_id))
 print('Initialization Finished')
-
-# ========================================
-#             Gradio Setting
-# ========================================
 
 def gradio_reset(chat_state, img_list):
     if chat_state is not None:
@@ -101,67 +92,29 @@ def gradio_answer(chatbot, chat_state, img_list, num_beams, temperature):
     chatbot[-1][1] = llm_message
     return chatbot, chat_state, img_list
 
-title = """<h1 align="center">Demo of MiniGPT-4</h1>"""
-description = """<h3>This is the demo of MiniGPT-4. Upload your images and start chatting!</h3>"""
-article = """<p><a href='https://minigpt-4.github.io'><img src='https://img.shields.io/badge/Project-Page-Green'></a></p><p><a href='https://github.com/Vision-CAIR/MiniGPT-4'><img src='https://img.shields.io/badge/Github-Code-blue'></a></p><p><a href='https://raw.githubusercontent.com/Vision-CAIR/MiniGPT-4/main/MiniGPT_4.pdf'><img src='https://img.shields.io/badge/Paper-PDF-red'></a></p>
-"""
+def anno(img_path,prompt,txt_path):
+    chatbot = gr.Chatbot(label='MiniGPT-4')
+    image=img_path
+    img_list = gr.State()
+    chat_state=gr.State()
+    text_input = gr.Textbox(label='User', placeholder='Please upload your image first', interactive=False)
 
-#TODO show examples below
+    image, text_input, upload_button, chat_state, img_list=upload_img(image, text_input, chat_state)
+    num_beams=5 # 1 to 10
+    temperature=2.0 # 0.1 to 2.0
+    text_input1, chatbot, chat_state=gradio_ask(prompt, [], chat_state)
+    chatbot, chat_state, img_list=gradio_answer(chatbot, chat_state, img_list, num_beams, temperature)
+    with open (txt_path + "/minigpt_anno.txt", 'w') as f :
+        f.write(chatbot[-1][1])
+    print(chatbot[-1][1])
+    gradio_reset(chat_state, img_list)
 
-with gr.Blocks() as demo:
-    gr.Markdown(title)
-    gr.Markdown(description)
-    gr.Markdown(article)
-
-    with gr.Row():
-        with gr.Column(scale=0.5):
-            image = gr.Image(type="pil")
-            upload_button = gr.Button(value="Upload & Start Chat", interactive=True, variant="primary")
-            clear = gr.Button("Restart")
-            
-            num_beams = gr.Slider(
-                minimum=1,
-                maximum=10,
-                value=1,
-                step=1,
-                interactive=True,
-                label="beam search numbers)",
-            )
-            
-            temperature = gr.Slider(
-                minimum=0.1,
-                maximum=2.0,
-                value=1.0,
-                step=0.1,
-                interactive=True,
-                label="Temperature",
-            )
-
-        with gr.Column():
-            chat_state = gr.State()
-            img_list = gr.State()
-            chatbot = gr.Chatbot(label='MiniGPT-4')
-            text_input = gr.Textbox(label='User', placeholder='Please upload your image first', interactive=False)
-    
-    upload_button.click(upload_img, [image, text_input, chat_state], [image, text_input, upload_button, chat_state, img_list])
-    
-    text_input.submit(gradio_ask, [text_input, chatbot, chat_state], [text_input, chatbot, chat_state]).then(
-        gradio_answer, [chatbot, chat_state, img_list, num_beams, temperature], [chatbot, chat_state, img_list]
-    )
-    clear.click(gradio_reset, [chat_state, img_list], [chatbot, image, text_input, upload_button, chat_state, img_list], queue=False)
-
-    demo.launch(share=True, enable_queue=True)
-# image=cv2.imread("/home/vipuser/zy_xr/MiniGPT-4/1.png",-1)[:,:,[2,1,0]]
-
-# chatbot = gr.Chatbot(label='MiniGPT-4')
-# image="/home/vipuser/zy_xr/MiniGPT-4/1.png"
-# img_list = gr.State()
-# chat_state=gr.State()
-# text_input = gr.Textbox(label='User', placeholder='Please upload your image first', interactive=False)
-
-# image, text_input, upload_button, chat_state, img_list=upload_img(image, text_input, chat_state)
-# num_beams=1 # 1 to 10
-# temperature=1 # 0.1 to 2.0
-# text_input1, chatbot, chat_state=gradio_ask("tell me what happen?", [], chat_state)
-# chatbot, chat_state, img_list=gradio_answer(chatbot, chat_state, img_list, num_beams, temperature)
-# print(chatbot[-1][1])
+root_dir = "/mnt/data1/zy/RLBench_data/Test_AllVariations_011701/"
+task_name = "close_box100243"
+eposides_path = root_dir + task_name + "/all_variations/episodes/episode" 
+# prompt = "The picture shows a robotic arm with gripper, and the door of the microwave oven on the table has been opened. Please describe in deatail how the robotic arm closes the the door of microwave. And the answer should focus on the specific operation of the robotic arm, such as how to rotate, when to close the gripper, and when to release the gripper etc. It is also required that the answer must only be listed in points such as 1.2.3.4 etc."
+# prompt = "The picture shows a robotic arm with gripper, and the lid of the laptop on the table has been opened. Please describe in deatail how the robotic arm closes the the lid of laptop. And the answer should focus on the specific operation of the robotic arm, such as how to rotate, when to close the gripper, and when to release the gripper etc. It is also required that the answer must only be listed in points such as 1.2.3.4 etc."
+prompt = "The picture shows a robotic arm with gripper, and the lid of the box on the table has been opened. Please describe in deatail how the robotic arm closes the the lid of box. And the answer should focus on the specific operation of the robotic arm, such as how to rotate, when to close the gripper, and when to release the gripper etc. It is also required that the answer must only be listed in points such as 1.2.3.4 etc."
+for i in range (0,2):
+    eposide_path = eposides_path + str(i)
+    anno(eposide_path + "/front_rgb/7.png", prompt, eposide_path)
